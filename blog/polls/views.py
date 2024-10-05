f292789ecc476cc5b0745f00dfd48ea31f6c3d79
qSlider <m.strelecc@gmail.com>
2024-10-05 15:11:31 +0300

    HW2

--- blog/polls/views.py
+++ blog/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+def index(request):
+    notes = ['Note 1', 'Note 2', 'Note 3']
+    return render(request, 'polls/index.html', {'notes': notes})
